ca8b5a56d68385e962747b26f084abab68335b9f
Harsh26 <harshbjw26@gmail.com>
2022-01-27 22:36:06 +0530

    Read arrival array in Question 1

--- Assignment_1/Question_1.cpp
+++ Assignment_1/Question_1.cpp
@@ -1,1 +1,63 @@
 // Write Code Here for Solution to Question 1... From Harsh's VS Studio Code 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> getArrivalTime()
+{
+    const int nCol = 10; // read from file
+
+    vector<vector<int>> v;
+
+    ifstream inFile;
+    inFile.open("arrival.txt");
+
+    if (inFile.fail()) 
+    {
+        cerr << "Could not find the ARRIVAL file" << endl;
+        exit(0);
+    }
+
+    // reads file to end of *file*, not line 
+
+    while (!inFile.eof()) 
+    {
+        vector<int> f; 
+
+        for (int i = 0; i < nCol; i++) 
+        {
+            int x;
+            inFile >> x;
+            f.push_back(x); 
+        }
+
+        v.push_back(f);
+        
+    }
+
+    return v;
+}
+
+void print2DVector(vector<vector<int>> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {   
+            cout << v[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+}
+int main()
+{
+    vector<vector<int>> arrival_time;
+
+    arrival_time=getArrivalTime();
+
+    print2DVector(arrival_time);
+
+    return 0;
+}
