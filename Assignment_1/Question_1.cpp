3b30badcdf6a2a848d6501a5b66d24656fdef3af
Harsh26 <harshbjw26@gmail.com>
2022-01-28 13:55:32 +0530

    Arrival time reading without hard-coding.

--- Assignment_1/Question_1.cpp
+++ Assignment_1/Question_1.cpp
@@ -4,36 +4,64 @@
 
 using namespace std;
 
+vector<int> extractIntegersFromLine(string str)
+{
+    vector<int> v;
+
+    char *p = &str[0];
+
+    while (*p) 
+    { // While there are more characters to process...
+
+        if ( isdigit(*p) || ( (*p=='-'||*p=='+') && isdigit(*(p+1)) )) 
+        {
+            // Found a number
+            long val = strtol(p, &p, 10); // Read number
+            //printf("%ld\n", val);        and print it.
+
+            v.push_back(val);
+        } 
+
+        else 
+        {
+            // Otherwise, move on to the next character.
+            p++;
+        }
+
+    }
+
+    return v;
+}
+
 vector<vector<int>> getArrivalTime()
 {
-    const int nCol = 10; // read from file
-
     vector<vector<int>> v;
 
+    string line;
+
     ifstream inFile;
+
     inFile.open("arrival.txt"); // File present in same folder
 
-    if (inFile.fail()) 
+    if (inFile.fail()) // File NOT Found
     {
         cerr << "Could not find the ARRIVAL file" << endl;
         exit(0);
     }
 
-    // reads file to end of *file*, not line 
 
-    while (!inFile.eof()) 
+    while (true) 
     {
+        if(!inFile) // reads file till end of *file*
+        break;
+
         vector<int> f; 
 
-        for (int i = 0; i < nCol; i++) 
-        {
-            int x;
-            inFile >> x;
-            f.push_back(x); 
-        }
+        getline(inFile,line);
+
+        f=extractIntegersFromLine(line);
 
         v.push_back(f);
-        
     }
 
     return v;
@@ -49,8 +77,8 @@ void print2DVector(vector<vector<int>> v)
         }
         cout<<"\n";
     }
-
 }
+
 int main()
 {
     vector<vector<int>> arrival_time;
