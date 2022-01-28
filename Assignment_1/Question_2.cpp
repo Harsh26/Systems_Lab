065056351e050ea734f395aa75c569ae6cc0800d
Harsh26 <harshbjw26@gmail.com>
2022-01-28 14:00:01 +0530

    Check commit

--- Assignment_1/Question_2.cpp
+++ Assignment_1/Question_2.cpp
@@ -1,1 +1,12 @@
 // Write Code Here for Solution to Question 2... From Harsh's VS Studio Code
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Start Code here Question 2 Systems Lab";
+
+    return 0;
+}
--- Assignment_1/Question_3.cpp
+++ Assignment_1/Question_3.cpp
@@ -1,1 +1,12 @@
 // Write Code Here for Solution to Question 3... From Harsh's VS Studio Code
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Start Code here Question 3 Systems Lab";
+
+    return 0;
+}
