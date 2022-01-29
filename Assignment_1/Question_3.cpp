9e7b3b2c79587542e6a4c85717d793cc6fa2fed8
Kishore <69838522+kishore7eng@users.noreply.github.com>
2022-01-29 15:40:55 +0530

    Update Question_3.cpp

--- Assignment_1/Question_3.cpp
+++ Assignment_1/Question_3.cpp
@@ -1,12 +1,61 @@
-// Write Code Here for Solution to Question 3... From Harsh's VS Studio Code
-
+#include <iostream>
 #include<bits/stdc++.h>
-
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-
-int main()
+int main(int argc, char** argv) 
 {
-    cout<<"Start Code here Question 3 Systems Lab";
+	
+	int n, m;
+	ifstream infile;
 
-    return 0;
+    infile.open("input.txt");
+    
+    if (infile.fail()) // File NOT Found
+    {
+        cerr << "Could not find the input file" << endl;
+        exit(0);
+    }
+    
+    infile>>n;
+    infile>>m;
+    cout<<n<<" "<<m<<endl;
+    
+	vector<int> available(m);
+	int x;
+	
+	for(int i = 0; i < m; i++)
+	{
+		infile>>x;
+		available.push_back(x);
+	}
+	
+	vector<vector<int>>allocated;
+	vector<vector<int>>maxReq;
+	
+	for(int i = 0; i < n; i++)
+	{
+		vector<int>temp;
+		for(int j = 0; j < m; j++)
+		{
+			infile>>x;
+			temp.push_back(x);
+		}
+		allocated.push_back(temp);
+		//cout<<allocated.size()<<" ";
+	}
+	cout<<"\n";
+	
+	for(int i = 0; i < n; i++)
+	{
+		vector<int>temp;
+		for(int j = 0; j < m; j++)
+		{
+			infile>>x;
+			temp.push_back(x);
+		}
+		maxReq.push_back(temp);
+		//cout<<maxReq.size()<<" ";
+	}
+	
+	return 0;
 }
