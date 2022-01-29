a73319c8f97f05c14c77d9cfe2e68960b19539a1
Harsh26 <harshbjw26@gmail.com>
2022-01-29 13:00:08 +0530

    Taken Input Coupon Num and Basic Error Checking

--- Assignment_1/Question_2.cpp
+++ Assignment_1/Question_2.cpp
@@ -1,12 +1,74 @@
-// Write Code Here for Solution to Question 2... From Harsh's VS Studio Code
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int string_to_int(string str)
+{
+    // object from the class stringstream
+    stringstream ss(str);
+
+    int num = 0;
+    ss >> num;
+
+    return num;
+}
+
 int main()
 {
-    cout<<"Start Code here Question 2 Systems Lab";
+    int n;
+
+    cout<<"\n\n\nEnter Number of Friends: ";
+    cin>>n;
+
+    vector<int> couponNumbers(n);
+
+    cout<<"\n\n";
+
+    for(int person=0;person<n;person++)
+    {
+        string cno;
+        vector<int> :: iterator itr;
+
+        cout<<"Enter 3 Digit Coupon Number for person "<<person+1<<" : ";
+        cin>>cno;
+
+        if(cno.length() != 3)
+        {
+            cout<<"NOT a 3 Digit Number, Enter Again...\n\n";
+            person--;
+            continue;
+        }
+
+        int coupNum = string_to_int(cno);
+
+        itr = std::find (couponNumbers.begin(), couponNumbers.end(), coupNum);
+
+        if (itr != couponNumbers.end())
+        {
+            cout << "Duplicate Coupon Number Found !! ";
+
+            person--;
+            cout<<"Enter Again...\n\n";
+            continue;
+        }
+        else
+        {
+            couponNumbers[person]=(coupNum);
+            cout << "Successfully Noted person "<<person+1<<" Coupon Number...\n\n";
+        }
+
+    }
+
+    cout<<"\n\nFollowing are Coupon Numbers :\n";
+
+    for(int person=0;person<n;person++)
+    {
+        cout<<"Coupon Number of person "<<person+1<<" = "<<couponNumbers[person]<<"\n";
+    }
 
     return 0;
 }
