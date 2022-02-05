a49a3a371013104a8e0f4c944bae2a35c7bf64fb
Kishore <69838522+kishore7eng@users.noreply.github.com>
2022-02-05 15:17:17 +0530

    Update Question_3.cpp

--- Assignment_1/Question_3.cpp
+++ Assignment_1/Question_3.cpp
@@ -15,12 +15,14 @@ pthread_mutex_t mutex;
 pthread_cond_t cond;
 
 int n, m,processDone;
-vector<int> availRes;
-vector<vector<int>> allocatedRes; 
-vector<vector<int>> maxRes ;
-vector<vector<int>> needRes;
+vector<int> availRes;                //stores available resources
+vector<vector<int>> allocatedRes;    //stores resource allocated to each process
+vector<vector<int>> maxRes ;         //stores maximum need of resources for each process
+vector<vector<int>> needRes;         //stores the amount of resource needed for each process to execute
 vector<int> seq;
 
+
+//reading input file
 void readInput()
 {
 	ifstream infile;
@@ -86,84 +88,84 @@ void readInput()
 }
 void *procs(void* processID)
 {     
-	int pID= ((intptr_t)processID); 
-	pthread_mutex_lock(&mutex);
+	int pID;
+	pthread_mutex_lock(&mutex);  //locking the thread
 	
-	while(pID != seq[processDone])
+	for(pID = ((intptr_t)processID);pID != seq[processDone];)
 	{
 		pthread_mutex_unlock(&mutex);
- 	    pthread_cond_wait(&cond, &mutex);
+ 	        pthread_cond_wait(&cond, &mutex);  //conditional wait ie a thread waits until the previous thread which entered the critical section is executued
  	}
 	
 
-	cout<<"\n--> Process: "<<pID+1;
-    cout<<"\n\tAllocated :  ";
-    for(int i=0; i<m; i++)
-        cout<< allocatedRes[pID][i]<<"  ";
+    	cout<<"\n--> Process: "<<pID+1;
+    	cout<<"\n\tAllocated :  ";
+    	for(int i=0; i<m; i++)
+        	cout<< allocatedRes[pID][i]<<"  ";
 
-    cout<<"\n\t"<<"Needed    :  ";
-    for(int i=0; i<m; i++)
-    	cout<<needRes[pID][i]<<"  ";
+    	cout<<"\n\t"<<"Needed    :  ";
+    	for(int i=0; i<m; i++)
+    		cout<<needRes[pID][i]<<"  ";
 
-    cout<<"\n\tAvailable :  ";
-    for(int i=0; i<m; i++)
-        cout<<availRes[i]<<"  ";
+    	cout<<"\n\tAvailable :  ";
+    	for(int i=0; i<m; i++)
+       		cout<<availRes[i]<<"  ";
 
-    cout<<endl;
+    	cout<<endl;
 	sleep(1);
-    cout<<"\tResource Allocated!";
-    cout<<endl; 
+    	cout<<"\tResource Allocated!";
+    	cout<<endl; 
 	sleep(1);
-    cout<<"\tResource Released!";
+    	cout<<"\tResource Released!";
 
 	for(int i=0; i<m; i++)
-        availRes[i] += allocatedRes[pID][i];
+        	availRes[i] += allocatedRes[pID][i];
 
-    cout<<"\n\tNow Available : ";
-    for(int i=0; i<m; i++)
-        cout<< availRes[i]<<"  ";
-    cout<<endl;
-    cout<<endl;
+    	cout<<"\n\tNow Available : ";
+    	for(int i=0; i<m; i++)
+        	cout<< availRes[i]<<"  ";
+    	cout<<endl;
+    	cout<<endl;
 	
-    processDone++;
-    pthread_cond_broadcast(&cond);
-    pthread_mutex_unlock(&mutex);
+    	processDone++;
+    	pthread_cond_broadcast(&cond);
+    	pthread_mutex_unlock(&mutex);//releasing lock
 	pthread_exit(NULL);
-    return NULL;
+    	return NULL;
 }
 int checkSafe()
 {
-    vector<bool>completed(n,false); 
-    vector<int>tempRes(availRes.begin(),availRes.end());
+    vector<bool>completed(n,false);    //boolean array to store whether a process is completed or not
+    vector<int>tempRes(availRes.begin(),availRes.end()); // temp array which stores available resources
     for(int i = 0; i < n; i++)
     {
         if (completed[i] == false)
         {
             for(int j = 0; j < m; j++)
             {
-                if(needRes[i][j] <= tempRes[j])
+                if(needRes[i][j] <= tempRes[j]) //if available resource is sufficient to need of a process then we allocate res to it
                 { 
-					if(j == m-1)  
-					{ 
-                        for (int k = 0; k < m; ++k)
-                        {
-                            tempRes[k] += allocatedRes[i][k];
-                    	}
-                    	completed[i] = true;
-                    	seq.push_back(i);
-						i = -1;
-						break;
-     			    }
-				}
-				else
+			if(j == m-1)  
+			{ 
+                        	for (int k = 0; k < m; ++k)
+                        	{
+                            		tempRes[k] += allocatedRes[i][k];
+                    		}
+                    		completed[i] = true;
+                    		seq.push_back(i);
+				i = -1;
 				break;
-			}
+     			  }
 		}
+		else
+		     break;
+	    }
 	}
+    }
            
     for(int i = 0; i < n; i++)
     {
-        if (completed[i] == false)
+        if (completed[i] == false) // if any of the process is not completed then return -1
             return -1;
     }        
     return 0;
@@ -182,28 +184,28 @@ int main()
 	{
 		cout<<seq[i] + 1<<"  ";
 	}
-
+	//creating threads to allocate resources according to safe sequence found
     pthread_attr_t attrDefault;
     pthread_attr_init(&attrDefault);
     pthread_t tid[n];
-	int pid[n];
+    int pid[n];
     
     for(int i = 0; i < n; i++)
     {
-	    pid[i] = i;
-	}
+	    pid[i] = i;//allocating process id to every threads
+    }
 
-	cout<<"\n";
+    cout<<"\n";
 
-	for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        pthread_create(&tid[i], NULL, procs, (void *) (intptr_t) pid[i]);
-	}
+        pthread_create(&tid[i], NULL, procs, (void *) (intptr_t) pid[i]);  //creating threads by passing process id to allocate resources
+    }
 	
-	for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-	    pthread_join(tid[i],NULL);
-	}
-	printf("\nAll Processes Finished\n");
+       pthread_join(tid[i],NULL);  //it makes thread wait until it terminates
+    }
+    printf("\nAll Processes Finished\n");
     return 0;
 }
