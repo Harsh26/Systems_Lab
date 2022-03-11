6ec32a5d72b2220510a4b6caf2e19f4440327e6c
Kishore <69838522+kishore7eng@users.noreply.github.com>
2022-03-11 09:23:44 +0530

    Update Question_1.cpp

--- Assignment_2/Question_1.cpp
+++ Assignment_2/Question_1.cpp
@@ -1,40 +1,63 @@
 #include <iostream>
 #include<vector>
+#include<fstream>
+#include<unordered_map>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-int main(int argc, char** argv) {
-	
-	int n1,n2, n3,noOfReq;
-	cout<<"Enter number of Single Occupancy rooms : ";
-	cin>>n1;
-	cout<<"Enter number of Double Occupancy rooms : ";
-	cin>>n2;
-	cout<<"Enter number of Triple Occupancy rooms : ";
-	cin>>n3;
-	
-	
-	cout<<"\nEnter number of Booking requests : ";
-	cin>>noOfReq;
-	cout<<endl;
-	
-	vector<int>requestEntries;
-	vector<int>blockSize;
+int n1,n2, n3,noOfReq;
+vector<int>requestEntries;
 
-	int profit[3] = {0},allocation[noOfReq] ;
-	char x;
+void readInput()
+{
+    ifstream infile;
+
+	string filename;
+
+	cout<<"\n\nEnter name of the file with .txt extension: ";
+    cin>>filename;
+
+    cout<<"\n\n";
+
+    infile.open(filename); // File present in same folder
+
+    if (infile.fail()) // File NOT Found
+    {
+        cerr << "Could not find the "<<filename<<".txt file\n" << endl;
+        cerr << "HINT: You may have missed \" .txt \" extension while writing.\n\n";
+        exit(0);
+    }
+
+    // Reading input...
 	
+	
+    infile>>n1;
+    infile>>n2;
+    infile>>n3;
+    infile>>noOfReq;
+
+   
+	char x;
 	for(int i = 0; i < noOfReq; i++)
 	{
-		cout<<"Enter request entry "<<i + 1<<" : ";
-		cin>>x;
-		if(x == 'T' || x ==  't')
+		infile>>x;
+		if(x == 'T')
 		requestEntries.push_back(3);
-		else if(x == 'D' || x == 'd')
+		else if(x == 'D')
 		requestEntries.push_back(2);
 		else
 		requestEntries.push_back(1);
 		
 	}
+	infile.close();
+}
+int main(int argc, char** argv) {
+	
+	
+	readInput();
+	
+	vector<int>blockSize;
+	int profit[3] = {0},allocation[noOfReq] ;
+	
 	for(int i = 0; i < n3; i++)
 	{
 		blockSize.push_back(3);
@@ -53,15 +76,17 @@ int main(int argc, char** argv) {
 	int flags[noOfReq] = {0},misAlloc[2];
 	misAlloc[0] = 0;
 	misAlloc[1] = 0;
-	
 	for(int i = 0; i < noOfBlocks; i++)
 	{
 		allocation[i] = -1;
 	}
+	
+	//first fit
 	for(int i = 0; i < noOfReq; i++)
 	{
 		for(int j = 0; j < noOfBlocks; j++)
 		{
+			//finding first block size >= required size
 			if(allocation[j] == -1 && blockSize[j] >= requestEntries[i])
 			{
 				if(blockSize[j] != requestEntries[i])
@@ -82,6 +107,7 @@ int main(int argc, char** argv) {
 	for(int i = 0; i <= 2; i++)
 	count[i] = 0;
 	
+	//checking number of rooms misallocated
 	for(int i = 0; i < noOfReq; i++)
 	{
 		if(flags[i] != 1)
@@ -96,18 +122,6 @@ int main(int argc, char** argv) {
 		} 
 	}
 	
-/*	
-	printf("\nBlock no.\tsize\t\tprocess no.\t\t");
-	for(int i = 0; i < noOfBlocks; i++)
-	{
-		printf("\n%d\t\t%d\t\t", i+1, blockSize[i]);
-		if(flags[i] == 1)
-			printf("%d\t\t\t",allocation[i]+1);
-		else
-			printf("Not allocated");
-	}
-	cout<<"\nProfit by Aman  "<<profit[0]<<endl;
-	cout<<"\n\n\n";*/
 	
 	for(int i = 0; i < noOfBlocks; i++)
 	{
@@ -118,8 +132,10 @@ int main(int argc, char** argv) {
 	{
 		flags[i] = 0;
 	}
+	
 	misAlloc[0] = 0;
 	misAlloc[1] = 0;
+	//best fit
 	for(int i = 0; i < noOfReq; i++)
 	{
 		int bestIdx = -1;
@@ -147,6 +163,7 @@ int main(int argc, char** argv) {
 				profit[1] += 4000;
         }
 	}
+	//checking number of misallocated rooms
 	for(int i = 0; i < noOfReq; i++)
 	{
 		if(flags[i] != 1)
@@ -154,26 +171,15 @@ int main(int argc, char** argv) {
 			if(misAlloc[requestEntries[i] - 2] > 0)
 			{
 			misAlloc[requestEntries[i] - 2]--;
-			count[1]++;
+			//count[1]++;
 			}
 			
 		} 
 	}
-/*	printf("\nBlock no.\tsize\t\tprocess no.");
-	for(int i = 0; i < noOfBlocks; i++)
-	{
-		printf("\n%d\t\t%d\t\t", i+1, blockSize[i]);
-		if(flags[i] == 1)
-			printf("%d\t\t\t",allocation[i]+1);
-		else
-			printf("Not allocated");
-	}
-	cout<<"\nProfit by Alok  "<<profit[1]<<endl;*/
 	
 	for(int i = 0; i < noOfBlocks; i++)
 	{
 		allocation[i] = -1;
-		//flags[i] = 0;
 	}
 	for(int i = 0; i < noOfReq; i++)
 	{
@@ -182,6 +188,7 @@ int main(int argc, char** argv) {
 	misAlloc[0] = 0;
 	misAlloc[1] = 0;
 	
+	//worst fit
 	for (int i = 0; i < noOfReq; i++)
     {
         // Find the best fit block for current process
@@ -210,6 +217,8 @@ int main(int argc, char** argv) {
 				profit[2] += 4000;
         }
     }
+    
+    //checking for misallotted rooms
     for(int i = 0; i < noOfReq; i++)
 	{
 		if(flags[i] != 1)
@@ -223,17 +232,7 @@ int main(int argc, char** argv) {
 			
 		} 
 	}
-	/*printf("\nBlock no.\tsize\t\tprocess no.");
-	for(int i = 0; i < noOfBlocks; i++)
-	{
-		printf("\n%d\t\t%d\t\t", i+1, blockSize[i]);
-		if(flags[i] == 1)
-			printf("%d\t\t\t",allocation[i]+1);
-		else
-			printf("Not allocated");
-	}
-	cout<<"\nProfit by Raj  "<<profit[2]<<endl;*/
-	
+	//finding maximum profit
     int max = INT_MIN,maxInd;
     for(int i = 0; i < 3; i++)
     {
@@ -243,8 +242,36 @@ int main(int argc, char** argv) {
     		maxInd = i;
 		}
 	}
-	string managers[3] = {"Aman","Alok","Raj"};
-	cout<<"\n\nManager Name \t\t Revenue Generated (Rs)\t\tMoney Wasted because of Nature of Manager (Rs) \t\tNo. of Request rejected because of wrong allotment of rooms";
+	ofstream outfile;
+	string fileName;
+	outfile.open("output.txt");
+	outfile<<"Manager Name \t\t Revenue Generated (Rs)\t\tMoney Wasted because of Nature of Manager (Rs) \t\tNo. of Request rejected because of wrong allotment of rooms\n\n";
+	for(int i = 0; i < 3; i++)
+    {
+    	if(i == 0)
+    	{
+    		outfile<<"\nAman\t\t\t\t"<<profit[i]<<"\t\t\t\t"<<max - profit[i]<<"\t\t\t\t\t\t"<<count[i];
+		}
+		else if(i == 1)
+    	{
+    		outfile<<"\nRaj\t\t\t\t"<<profit[i+1]<<"\t\t\t\t"<<max - profit[i+1]<<"\t\t\t\t\t\t"<<count[i+1];
+		}
+		else if(i == 2)
+    	{
+    		outfile<<"\nAlok\t\t\t\t"<<profit[i-1]<<"\t\t\t\t"<<max - profit[i-1]<<"\t\t\t\t\t\t"<<count[i-1];
+		}
+    }
+    switch(maxInd)
+    {
+    	case 0 : outfile<<"\nAman is best manager\n\n";
+    			break;
+    	case 1 : outfile<<"\nAlok is best manager\n\n";
+    			break;
+    	case 2 : outfile<<"\nRaj is best manager\n\n";
+    			break;
+	}
+	outfile.close();
+	cout<<"Manager Name \t\t Revenue Generated (Rs)\t\tMoney Wasted because of Nature of Manager (Rs) \t\tNo. of Request rejected because of wrong allotment of rooms\n\n";
 	for(int i = 0; i < 3; i++)
     {
     	if(i == 0)
@@ -260,6 +287,14 @@ int main(int argc, char** argv) {
     		cout<<"\nAlok\t\t\t\t"<<profit[i-1]<<"\t\t\t\t"<<max - profit[i-1]<<"\t\t\t\t\t\t"<<count[i-1];
 		}
     }
-    cout<<"\n"<<managers[maxInd]<<" is the best manager\n";
+    switch(maxInd)
+    {
+    	case 0 : cout<<"\nAman is best manager\n\n";
+    			break;
+    	case 1 : cout<<"\nAlok is best manager\n\n";
+    			break;
+    	case 2 : cout<<"\nRaj is best manager\n\n";
+    			break;
+	}
 	return 0;
 }
